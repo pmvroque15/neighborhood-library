d313405db149599cde9273f962c441ff168575d9
pmvroque15 <pmvroque@gmail.com>
2026-04-17 11:23:25 -0400

    Made constructor and set up arrays of 10 books

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,8 +1,29 @@
 package com.pluralsight;
 
 public class Book {
-    static void main() {
+    int id;
+    String isbn;
+    String title;
+    boolean isCheckedOut;
+    String checkedOUtTo;
 
-        }
+    static void main() {
+    }
+
+    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOUtTo) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+        this.checkedOUtTo = checkedOUtTo;
+    }
+
+    static void checkOut(String name) {
+
+    }
+    static void checkIn() {
+
+    }
+
     }
 
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
 public class Main {
+
+    static void main() {
+        Book[] books = new Book[] {
+                new Book(1, "978-0451524935", "1984", false, ""),
+                new Book(2, "978-0140449136", "The Odyssey", false, ""),
+                new Book(3, "978-0439139601", "Harry Potter and the Goblet of Fire", false, ""),
+                new Book(4, "978-0061120084", "To Kill a Mockingbird", false, ""),
+                new Book(5, "978-0307277671", "The Road", false, ""),
+                new Book(6, "978-0743273565", "The Great Gatsby", false, ""),
+                new Book(7, "978-1501128035", "It", false, ""),
+                new Book(8, "978-0553386790", "A Game of Thrones", false, ""),
+                new Book(9, "978-0060850524", "Brave New World", false, ""),
+                new Book(10, "978-1594633669", "The Kite Runner", false, "")
+        };
+    }
 }
