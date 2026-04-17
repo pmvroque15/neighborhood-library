88a0da0eb7c535067bb53751f0f803a2573b4924
pmvroque15 <pmvroque@gmail.com>
2026-04-17 14:06:07 -0400

    Added another prompt for the menu

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,19 +1,105 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
     static void main() {
-        Book[] books = new Book[] {
-                new Book(1, "978-0451524935", "1984", false, ""),
-                new Book(2, "978-0140449136", "The Odyssey", false, ""),
-                new Book(3, "978-0439139601", "Harry Potter and the Goblet of Fire", false, ""),
-                new Book(4, "978-0061120084", "To Kill a Mockingbird", false, ""),
-                new Book(5, "978-0307277671", "The Road", false, ""),
-                new Book(6, "978-0743273565", "The Great Gatsby", false, ""),
-                new Book(7, "978-1501128035", "It", false, ""),
-                new Book(8, "978-0553386790", "A Game of Thrones", false, ""),
-                new Book(9, "978-0060850524", "Brave New World", false, ""),
-                new Book(10, "978-1594633669", "The Kite Runner", false, "")
-        };
+        Book[] books = new Book[10];
+
+            books[0] = new Book(0, "978-0451524935", "1984", false, "");
+            books[1] = new Book(1, "978-0140449136", "The Odyssey", false, "");
+            books[2] = new Book(2, "978-0439139601", "Harry Potter and the Goblet of Fire", false, "");
+            books[3] = new Book(3, "978-0061120084", "To Kill a Mockingbird", false, "");
+            books[4] = new Book(4, "978-0307277671", "The Road", false, "");
+            books[5] = new Book(5, "978-0743273565", "The Great Gatsby", false, "");
+            books[6] = new Book(6, "978-1501128035", "It", false, "");
+            books[7] = new Book(7, "978-0553386790", "A Game of Thrones", false, "");
+            books[8] = new Book(8, "978-0060850524", "Brave New World", false, "");
+            books[9] = new Book(9, "978-1594633669", "The Kite Runner", false, "");
+
+
+        String mainPrompt = """
+                =======MAIN MENU=======
+                1. Show available books
+                2. Show checked out books
+                3. Exit this application
+                =======================
+                
+                Please enter you selection.
+                """;
+
+        System.out.println(mainPrompt);
+        String userSelection = scanner.nextLine();
+
+        switch (userSelection) {
+            case "1":
+                //available book menu
+                showAvailableMenu(books);
+                break;
+            case "2":
+                //checked out books menu
+                showCheckedOutMenu();
+                break;
+            case "3":
+                //exit
+                break;
+            default:
+                System.err.println("Invalid Selection");
+
+        }
     }
+
+    private static void showAvailableMenu(Book[] books){
+        System.out.println("========Available books===========");
+        System.out.println("ID\tISBN\t\tTitle");
+        for (Book currentBook : books) {
+            if (!currentBook.isCheckedOut) {
+                System.out.print(currentBook.id + " ");
+                System.out.print(currentBook.isbn + " ");
+                System.out.println(currentBook.title);
+            }
+        }
+        String prompt = """
+            1. Check out a book
+            2. Exit to menu
+            """;
+        System.out.println(" ");
+        System.out.println(prompt);
+        String userSelection = scanner.nextLine();
+
+        switch (userSelection) {
+            case "1":
+                //check out book
+                System.out.println("Enter the book ID number of the book you want to check out: ");
+                userSelection = scanner.nextLine();
+                int id = Integer.parseInt(userSelection);
+
+                System.out.println("Enter your name: ");
+                String name = scanner.nextLine();
+
+                books[id].isCheckedOut = true;
+                books[id].checkedOUtTo = name;
+
+//                books[id].checkOut("Pat");
+                break;
+            case "2":
+                //exit
+                break;
+            default:
+                System.err.println("Invalid Selection");
+
+        }
+    }
+
+    public Book[] checkOut(Book[] books) {
+        this.isCheckedOut = true;
+        this.checkedOutTo = name;
+    }
+    private static void showCheckedOutMenu() {
+
+    }
+
 }
+
